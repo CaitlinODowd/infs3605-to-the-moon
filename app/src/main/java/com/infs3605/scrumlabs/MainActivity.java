03660aaf2fea31bba60021cb6998da91435467a3
Sharon Cheung <sharoncheung121998@gmail.com>
2021-03-27 17:01:58 +0800

    added new app and To The Moon logos and updated the splash screen with animation

--- app/src/main/java/com/infs3605/scrumlabs/MainActivity.java
+++ app/src/main/java/com/infs3605/scrumlabs/MainActivity.java
@@ -3,17 +3,23 @@ package com.infs3605.scrumlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.infs3605.scrumlabs.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Comment by Connor
-        //comment by sharon
+
+
     }
 }
 
--- app/src/main/java/com/infs3605/scrumlabs/SplashActivity.java
+++ app/src/main/java/com/infs3605/scrumlabs/SplashActivity.java
@@ -3,17 +3,45 @@ package com.infs3605.scrumlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    //Variables for splash screen animation
+    Animation topAnim, bottomAnim;
+    ImageView ivAppLogo, ivRedCrossLogo, ivOurLogo;
+    TextView tvAppName, tvTagline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //to hide status bar for splash screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         (new Handler()).postDelayed(this::launchLoginActivity, 5000);
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        ivAppLogo =findViewById(R.id.imLogo);
+        ivRedCrossLogo =findViewById(R.id.imRedCross);
+        ivOurLogo=findViewById(R.id.imToTheMoon);
+        tvAppName =findViewById(R.id.tvSplash);
+        tvTagline =findViewById(R.id.tv_tagline);
+
+        ivAppLogo.setAnimation(topAnim);
+        ivRedCrossLogo.setAnimation(bottomAnim);
+        ivOurLogo.setAnimation(bottomAnim);
+        tvAppName.setAnimation(topAnim);
+        tvTagline.setAnimation(topAnim);
+
     }
 
     private void launchLoginActivity() {
