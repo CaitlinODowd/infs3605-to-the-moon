8d3653a6da42e4234c0d736fad71998af7beb9e6
UNSWConnor <z5189800@ad.unsw.edu.au>
2021-04-11 23:44:25 +1000

    Added exportSchema = false within UserDatabase.java to prevent build warning

--- app/src/main/java/com/infs3605/tothemoon/UserDatabase.java
+++ app/src/main/java/com/infs3605/tothemoon/UserDatabase.java
@@ -3,7 +3,7 @@ package com.infs3605.tothemoon;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class UserDatabase extends RoomDatabase {
     public abstract UserDao userDao();
 }
